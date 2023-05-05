015a1262bf7986d44c48ebc986a739fd771900df
PakYouMu <21-1-00398@vsu.edu.ph>
2023-05-05 23:50:39 +0800

    Updated OpenAI usage. Still don't know how to store the results into text file. WIll look into that more at a later date.

--- The Stoic Way/TheStoicWay.cs
+++ The Stoic Way/TheStoicWay.cs
@@ -9,6 +9,7 @@ using Microsoft.Win32;
 using System.Timers;
 using OpenAI_API.Completions;
 using OpenAI_API;
+using System.IO;
 
 namespace The_Stoic_Way
 {
@@ -24,13 +25,29 @@ namespace The_Stoic_Way
         private void TheStoicWay_Load(object sender, EventArgs e)
         {
             var openAI = new OpenAIAPI("sk - xF4xGMEmYTcwCuSO67lCT3BlbkFJL6g1H06fcHSCLEZxAYvN");
-            CompletionRequest completionRequest = new CompletionRequest
+            CompletionRequest request = new CompletionRequest();
+            request.Prompt = "I am a Stoic. A follower or practitioner of an ancient Philosophy developed  by the Greeks, and perfected by the Romans. It has long been since misattributed to being the philosophy of showing no emotion, however, in truth it is the philosophy and way of life that focuses in wisdom, justice, moderation, and courage. There have been multiple philosophers, men, women, leaders, slaves, and the such that have contributed to such philosophy over the thousands of years it has existed.Give me one Stoic quote.";
+            request.Model = OpenAI_API.Models.Model.DavinciText;
+            var completions = openAI.Completions.CreateCompletionAsync(request);
+
+            foreach(var completion in completions.Result.Completions)
             {
-                Prompt = "I am a Stoic. A follower or practitioner of an ancient Philosophy developed  by the Greeks, and perfected by the Romans. It has long been since misattributed to being the philosophy of showing no emotion, however, in truth it is the philosophy and way of life that focuses in wisdom, justice, moderation, and courage. There have been multiple philosophers, men, women, leaders, slaves, and the such that have contributed to such philosophy over the thousands of years it has existed. Give me one Stoic quote that reminds me how great life is",
+                string filePath = @"C:\quotes.txt";
+                using (StreamWriter writer = File.AppendText(filePath))
+                {
+                    writer.WriteLine(completion.Text);
+                }
+            }
+
+            
+
+            /*CompletionRequest completionRequest = new CompletionRequest
+            {
+                Prompt = "I am a Stoic. A follower or practitioner of an ancient Philosophy developed  by the Greeks, and perfected by the Romans. It has long been since misattributed to being the philosophy of showing no emotion, however, in truth it is the philosophy and way of life that focuses in wisdom, justice, moderation, and courage. There have been multiple philosophers, men, women, leaders, slaves, and the such that have contributed to such philosophy over the thousands of years it has existed. Give me one Stoic quote.",
 
                 Model = "text-davinci-003",
-                MaxTokens = 1000,
-            };
+                MaxTokens = 100,
+            };*/
         }
 
         private void TheStoicWay_FormClosing(object sender, FormClosingEventArgs e)
