6d77fda401c0348d2e2722845c93bb7de918aa2e
PakYouMu <21-1-00398@vsu.edu.ph>
2023-06-07 23:37:08 +0800

    Pre-Update to WiX v4 Checkpoint

--- The Stoic Way/Classes/Program.cs
+++ The Stoic Way/Classes/Program.cs
@@ -1,7 +1,27 @@
 using System.Timers;
+using Microsoft.Win32;
 
 namespace The_Stoic_Way.Classes
 {
+    public class AutostartManager
+    {
+        private const string AppName = "The Stoic Way";
+        private const string RunKey = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run";
+
+        public static void AddToAutostart()
+        {
+            RegistryKey startupKey = Registry.CurrentUser.OpenSubKey(RunKey, true);
+            startupKey.SetValue(AppName, System.Reflection.Assembly.GetExecutingAssembly().Location);
+            startupKey.Close();
+        }
+
+        public static void RemoveFromAutostart()
+        {
+            RegistryKey startupKey = Registry.CurrentUser.OpenSubKey(RunKey, true);
+            startupKey.DeleteValue(AppName, false);
+            startupKey.Close();
+        }
+    }
     internal static class Program
     {
         /// <summary>
@@ -12,6 +32,12 @@ namespace The_Stoic_Way.Classes
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
+
+            // Add to autostart
+            AutostartManager.AddToAutostart();
+
+            // Remove from autostart
+            // AutostartManager.RemoveFromAutostart();
             ApplicationConfiguration.Initialize();
             Application.Run(new TheStoicWay());
         }
